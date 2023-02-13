f2cfeb8d8e11503836ff59b11c136d06692cadf7
Thiago Kichiro <kichiro.88@gmail.com>
2023-02-13 01:35:20 -0300

    Initial commit branch main

---
+++ backend/src/main/java/com/tkdev/desafiocap01/desafiocap01/Desafiocap01Application.java
@@ -0,0 +1,13 @@
+package com.tkdev.desafiocap01.desafiocap01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafiocap01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafiocap01Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tkdev/desafiocap01/desafiocap01/Desafiocap01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkdev.desafiocap01.desafiocap01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafiocap01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
